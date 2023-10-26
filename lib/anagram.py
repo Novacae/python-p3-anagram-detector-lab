a2d35bdd6dfb91d5150158248f3659de3c744526
aman githinji <amangithinji09@gmail.com>
2023-10-26 16:08:40 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,25 @@
 # your code goes here!
+class Anagram :
+    def __init__(self,word):
+        self.word = word.lower()
+
+    
+    def match(self, candidates):
+        matches = []
+        sorted_word = sorted(self.word)
+
+        for candidate in candidates:
+            sorted_candidate = sorted(candidate.lower())
+
+            if sorted_candidate == sorted_word and candidate.lower() != self.word:
+                matches.append(candidate)
+
+        return matches
+
+
+   #def match():
+        f#rom collections import Counter
+        #if Counter(self.word) == Counter(self):
+         #   return self
+        #else:
+        #    return []
